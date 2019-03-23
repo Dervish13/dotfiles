31b619a64ba5b86494dbd330d4b51d8dd15b4283
Rajko Dervisevic <rajkod@hotmail.com>
2019-03-23 20:02:43 +0100

    minor changes

--- xconfig/bin/init.sh
+++ xconfig/bin/init.sh
@@ -3,6 +3,6 @@
 ROOT_DIR="$(readlink -f $(dirname $0)/..)"
 
 rm -rf ~/.xmodmap ~/.xinitrc ~/.Xdefaults
-cp "${ROOT_DIR}/xmodmap" ~/.xmodmap
+#cp "${ROOT_DIR}/xmodmap" ~/.xmodmap
 cp "${ROOT_DIR}/xinitrc" ~/.xinitrc
 cp "${ROOT_DIR}/Xdefaults" ~/.Xdefaults
