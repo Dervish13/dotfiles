2601330e5795168f62364ea975b89ecc2aaef621
Rajko Dervisevic <dervish13@tilda.center>
2019-07-29 18:27:34 +0200

    xmodmap added

--- xconfig/bin/init.sh
+++ xconfig/bin/init.sh
@@ -2,7 +2,7 @@
 
 ROOT_DIR="$(readlink -f $(dirname $0)/..)"
 
-rm -rf ~/.xinitrc ~/.Xdefaults
-#cp "${ROOT_DIR}/xmodmap" ~/.xmodmap
+rm -rf ~/.xinitrc ~/.Xdefaults ~/.xmodmap
+ln "${ROOT_DIR}/.xmodmap" ~/.xmodmap
 ln "${ROOT_DIR}/.xinitrc" ~/.xinitrc
 ln "${ROOT_DIR}/.Xdefaults" ~/.Xdefaults
